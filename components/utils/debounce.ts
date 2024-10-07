25d296b6f03a0c3bf1d37cf70dfad1416c42d208
Bronislav Musil <brona.musil@gmail.com>
2024-10-07 18:16:19 +0200

    Spread

--- components/utils/debounce.ts
+++ components/utils/debounce.ts
@@ -2,6 +2,6 @@ export function debounce<T extends (...args: any[]) => any>(fn: T, delay: number
 	let timer: number;
 	return function (...args: Parameters<T>) {
 		window.clearTimeout(timer);
-		timer = window.setTimeout(() => fn.apply(null, args), delay);
-	}
+		timer = window.setTimeout(() => fn(...args), delay); // Use spread instead of apply
+	};
 }
