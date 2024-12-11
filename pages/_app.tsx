06b4c8a6edf387eacbe4f7d5ebae05e49fc7d79e
Bronislav Musil <brona.musil@gmail.com>
2024-12-11 21:35:55 +0100

    Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import '../styles/globals.scss';
 import {NextIntlClientProvider} from 'next-intl';
 import {useRouter} from 'next/router';
 import {OverlaysProvider} from '@blueprintjs/core';
+import { Analytics } from '@vercel/analytics/next';
 
 const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
     const router = useRouter();
@@ -15,6 +16,7 @@ const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
                 messages={pageProps.messages}
             >
                 <OverlaysProvider>
+                    <Analytics />
                     <Component key={router.route} {...pageProps} />
                 </OverlaysProvider>
             </NextIntlClientProvider>
