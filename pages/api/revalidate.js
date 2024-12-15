b2cd36084793a326507295eae779d609146733f5
Bronislav Musil <brona.musil@gmail.com>
2024-12-15 19:17:06 +0100

    Revalidates

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -54,8 +54,8 @@ export default async function handler(req, res) {
         }
 
         await Promise.all([
-            res.revalidate(path),
-            res.revalidate('/en' + path)
+            res.revalidate('/en' + path),
+            res.revalidate('/cs' + path)
         ]);
 
         return res.json({ revalidated: true });
