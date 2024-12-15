3cdadc800a9d61a910bccee2e9f7475ceae51c81
Bronislav Musil <brona.musil@gmail.com>
2024-12-15 13:11:42 +0100

    Localization in 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Layout from '../components/Layout';
 import React from 'react';
+import {GetStaticPropsContext} from 'next';
 
 export default function NotFound() {
 
@@ -11,4 +12,15 @@ export default function NotFound() {
             </section>
         </Layout>
     );
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+    return {
+        props: {
+            // You can get the messages from anywhere you like. The recommended
+            // pattern is to put them in JSON files separated by locale and read
+            // the desired one based on the `locale` received from Next.js.
+            messages: (await import(`../public/locales/${context.locale}.json`)).default
+        }
+    };
 }
